fefc3d537e09ebcdc4e509ac1aada70fb86db9bc
kaclok <572005238@qq.com>
2025-02-16 11:31:16 +0800

    // https://www.bilibili.com/video/BV1CnftYuEvQ?spm_id_from=333.788.videopod.sections&vd_source=5c9f5bd891aee351c325bcf632b5550f // 子组件接收父组件的所有数据，事件，插槽 // 子组件的"$attrs" 在模版中接收 父组件传递的：数据，事件 ，在<script setup> 中，需要使用 useAttrs 函数来获取 $attrs 对象 // 子组件的"$slots" 在模版中接收 父组件传递的：插槽

--- frontend/src/framework/utils/use/useVModel.js
+++ frontend/src/framework/utils/use/useVModel.js
@@ -1,6 +1,11 @@
 // https://www.bilibili.com/video/BV11LftYkEm7?spm_id_from=333.788.videopod.sections&vd_source=5c9f5bd891aee351c325bcf632b5550f
 // 防止子组件通过v-model修改父组件传递的参数
 
+// https://www.bilibili.com/video/BV1CnftYuEvQ?spm_id_from=333.788.videopod.sections&vd_source=5c9f5bd891aee351c325bcf632b5550f
+// 子组件接收父组件的所有数据，事件，插槽
+// 子组件的"$attrs" 在模版中接收 父组件传递的：数据，事件 ，在<script setup> 中，需要使用 useAttrs 函数来获取 $attrs 对象
+// 子组件的"$slots" 在模版中接收 父组件传递的：插槽
+
 import {computed} from "vue";
 
 function useVModel(props, propName, emit) {
