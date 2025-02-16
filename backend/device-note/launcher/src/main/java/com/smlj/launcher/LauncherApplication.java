3c49f9cb48c4e78ada13f1c239b0ba289084b64e
kaclok <572005238@qq.com>
2025-02-16 10:18:11 +0800

    vite.config.js中配置查看编译之后的vue代码 import inspect from 'vite-plugin-inspect' https://www.bilibili.com/video/BV1MmNJe3EpJ/?spm_id_from=333.1007.tianma.1-1-1.click&vd_source=5c9f5bd891aee351c325bcf632b5550f

--- backend/device-note/launcher/src/main/java/com/smlj/launcher/LauncherApplication.java
+++ backend/device-note/launcher/src/main/java/com/smlj/launcher/LauncherApplication.java
@@ -14,8 +14,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 // 有了 多数据源 和动态台数据源 需要屏蔽默认的数据源配置形式
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class/*, SecurityAutoConfiguration.class*/}/*, scanBasePackages = {"com.smlj"}*/)
 // 为什么扫描com.smlj就启动不了，扫描com.smlj.logic.o就可以
-/*@ComponentScan(basePackages = {"com.smlj"})*/
-@ComponentScan(basePackages = {"com.smlj.logic.o"})
+@ComponentScan(basePackages = {"com.smlj"})
+/*@ComponentScan(basePackages = {"com.smlj.logic.o"})*/
 public class LauncherApplication {
     public static void main(String[] args) {
         SpringApplication.run(LauncherApplication.class, args);
