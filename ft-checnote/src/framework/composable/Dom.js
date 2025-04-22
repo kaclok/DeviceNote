86557d66b14ee72fede90c13fd706d6cc34a9bb3
kaclok <572005238@qq.com>
2025-04-22 11:08:52 +0800

    vueUse 用组合式函数、非渲染组件、v指令

--- ft-checnote/src/framework/composable/Dom.js
+++
@@ -1,7 +0,0 @@
-import {onMounted, onUnmounted} from 'vue'
-
-export function useDomListener(target = window, event, callback) {
-    // 也可以用字符串形式的 CSS 选择器来寻找目标 DOM 元素
-    onMounted(() => target.addEventListener(event, callback))
-    onUnmounted(() => target.removeEventListener(event, callback))
-}
