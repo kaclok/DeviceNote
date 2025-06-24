a00ff35d4f2db032be09a60c2bfcb48fb69119d3
kaclok <572005238@qq.com>
2025-06-24 10:12:33 +0800

    smlj采购供应

---
+++ ft-checnote/src/cms/smlj/sbrhjy/App.vue
@@ -0,0 +1,155 @@
+<template>
+    <el-container class="layout-container-demo" style="height: 100%">
+        <el-aside width="80px;">
+            <el-scrollbar>
+                <el-menu :default-active="refMenu" unique-opened>
+                    <el-menu-item index="1">公辅装置</el-menu-item>
+                    <el-menu-item index="2">热动力站装置</el-menu-item>
+                    <el-menu-item index="3">烧碱装置</el-menu-item>
+                    <el-sub-menu index="4">
+                        <template #title>聚氯乙烯装置</template>
+                        <el-menu-item index="4-1">聚合</el-menu-item>
+                        <el-menu-item index="4-2">乙炔</el-menu-item>
+                        <el-menu-item index="4-3">VCM</el-menu-item>
+                        <el-menu-item index="4-4">固液分离</el-menu-item>
+                    </el-sub-menu>
+                </el-menu>
+            </el-scrollbar>
+        </el-aside>
+
+        <el-container>
+            <div>
+                <div class="flex flex-wrap gap-4 items-center">
+                    <el-select
+                        v-model="refSelect"
+                        placeholder="请选择:"
+                        style="width: 240px"
+                    >
+                        <el-option
+                            v-for="item in options"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value"
+                        />
+                    </el-select>
+                </div>
+
+                <el-main>
+                    <el-scrollbar>
+                        <el-table :data="tableData">
+                            <el-table-column prop="name" label="设备名称" width="200"/>
+                            <el-table-column prop="weihao" label="设备位号" width="200"/>
+                            <el-table-column prop="size" label="设备型号" width="200"/>
+                            <el-table-column prop="prevFixATime" label="上次轴伸端时间" width="180"/>
+                            <el-table-column prop="nextFixATime" label="下次轴伸端时间" width="180"/>
+                            <el-table-column prop="prevFixBTime" label="上次非轴伸端时间" width="180"/>
+                            <el-table-column prop="nextFixBTime" label="下次轴伸端时间" width="180"/>
+                        </el-table>
+                    </el-scrollbar>
+                </el-main>
+            </div>
+        </el-container>
+    </el-container>
+
+</template>
+
+<script lang="ts" setup>
+import {ref, watch} from 'vue'
+import {Menu as IconMenu, Message, Setting} from '@element-plus/icons-vue'
+
+// 默认选择高压电机
+const refSelect = ref("1")      // 下拉框默认选中高压电机
+const refMenu = ref("1")       // 菜单默认选中第一项
+
+const options = [
+    {
+        value: "1",
+        label: '高压电动机',
+    },
+    {
+        value: '2',
+        label: '低压电动机',
+    },
+    {
+        value: '3',
+        label: '免维护电机',
+    },
+]
+const item = {
+    name: '2016-05-02',
+    size: 'Tom',
+    weihao: 'Dom',
+    prevFixATime: '2025-04-25',
+    nextFixATime: '2025-06-25',
+    prevFixBTime: '2025-04-25',
+    nextFixBTime: '2025-06-25',
+}
+
+const tableData = ref([])
+
+function _reqList() {
+    doGet("x/wz/getList", {
+        group: curGroupIndex.value,
+        filterByName: filterName.value,
+        filterByModel: filterModel.value,
+        filterByDeclarer: filterDeclarer.value,
+        filterByHt: filterHt.value,
+        pageNum: curPageIndex.value,
+        pageSize: PAGE_SIZE
+    }, AC_getList.signal, () => {
+        loadingList.value = true;
+    }, (r, data) => {
+        loadingList.value = false;
+
+        if (r) {
+            formList.value = data.data.list
+            formTotal = data.data.total
+        } else {
+        }
+    })
+}
+
+watch([()=> refSelect.value, ()=> refMenu.value] => {
+
+})
+</script>
+
+<style scoped>
+.layout-container-demo .el-header {
+    position: relative;
+    background-color: var(--el-color-primary-light-7);
+    color: var(--el-text-color-primary);
+}
+
+.layout-container-demo .el-aside {
+    color: var(--el-text-color-primary);
+    background: #fff;
+    width: 160px;
+}
+
+.layout-container-demo .el-menu {
+    border-right: none;
+}
+
+.layout-container-demo .el-main {
+    padding-top: 10px;
+    padding-left: 0;
+}
+
+.layout-container-demo .toolbar {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    right: 20px;
+}
+
+/*https://element-plus.org/zh-CN/component/menu.html*/
+.el-menu--horizontal {
+    --el-menu-horizontal-height: 40px;
+}
+
+.el-menu--vertical {
+    --el-menu-horizontal-height: 30px;
+}
+</style>
