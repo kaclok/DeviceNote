1ede9b1f6f234cd3d9b4df394e31c6728349cc14
kaclok <572005238@qq.com>
2026-01-29 08:53:57 +0800

    给下载中心的各个app配置历史版本下载

--- ft-checnote/src/cms/downloads/App.vue
+++ ft-checnote/src/cms/downloads/App.vue
@@ -13,8 +13,19 @@
                 <div class="app-name">{{ item.name }}</div>
 
                 <button class="download-btn" @click="download(item)">
-                    下载
+                    下载 {{ item.latestVersion }}
                 </button>
+
+                <button @click="item.showVersions = !item.showVersions">
+                    历史版本
+                </button>
+
+                <ul v-if="item.showVersions">
+                    <li v-for="v in item.versions" :key="v.version">
+                        {{ v.version }}
+                        <a :href="v.downloadUrl">下载</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -30,13 +41,63 @@ const appList = ref([
         id: 1,
         name: 'NFC巡检',
         icon: logo_book,
-        downloadUrl: '/downloads/NFCPatrol-release-1.0.apk'
+        latestVersion: "1.0.0",
+        latestDownloadUrl: '/downloads/nfc-patrol/latest/NFCPatrol-release-1.0.apk',
+        versions: [
+            {
+                "version": "1.0.0",
+                "publishTime": "2026-01-20",
+                "downloadUrl": "/downloads/nfc-patrol/1.0.0/NFCPatrol-release-1.0.apk"
+            },
+            {
+                "version": "2.0.0",
+                "publishTime": "2025-12-01",
+                "downloadUrl": "/downloads/nfc-patrol/2.0.0/NFCPatrol-release-1.0.apk"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: '承包商apk',
+        icon: logo_book,
+        latestVersion: "1.1.26",
+        latestDownloadUrl: '/downloads/cbs/latest/cbs_1.1.26.apk',
+        versions: [
+            {
+                "version": "1.1.26",
+                "publishTime": "2026-01-20",
+                "downloadUrl": '/downloads/cbs/latest/cbs_1.1.26.apk'
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: '承包商认证img',
+        icon: logo_book,
+        latestVersion: "1.1.26",
+        latestDownloadUrl: '/downloads/cbs/latest/cbsQRCode.png',
+        versions: [
+            {
+                "version": "1.1.26",
+                "publishTime": "2026-01-20",
+                "downloadUrl": '/downloads/cbs/latest/cbsQRCode.png'
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: '工业互联网',
+        icon: logo_book,
+        latestVersion: "0.0.0",
+        latestDownloadUrl: '/downloads/gyhlw/latest/gyhlw.apk',
+        versions: [
+        ]
     }
 ])
 
 const download = (item) => {
     // 方式一：直接跳转下载（最简单）
-    window.open(item.downloadUrl)
+    window.open(item.latestDownloadUrl)
 
     // 或方式二：a 标签方式（可控文件名）
     // const a = document.createElement('a')
