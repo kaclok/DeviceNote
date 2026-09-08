cec3b2454940d9c8bbd8a75ce48aefcc6cf4987c
kaclok <572005238@qq.com>
2026-09-08 10:59:07 +0800

    const newArray = array.map((element, index, array) => {     // 处理每个元素     return 处理后的值; })

--- ft-checnote/src/framework/utils/MapUtil.js
+++ ft-checnote/src/framework/utils/MapUtil.js
@@ -1,6 +1,6 @@
 export class MapUtil {
     static Map2KVArray(map, kName = 'key', vName = 'value') {
-        return Object.entries(map).map(([key, value]) => ({
+        return Object.entries(map).map(([key, value], index, array) => ({
             [kName]: key,    // 使用 [] 动态设置属性名
             [vName]: value   // 使用 [] 动态设置属性名
         }))
