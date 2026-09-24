c169b64b5bbaff8617bdcf0e6e30eec2d26f85e9
kaclok <572005238@qq.com>
2026-09-25 07:55:09 +0800

    将前端mpa每个入口都配置一个api-xxx的baseURl, 根据这个配置vite.config.js中动态生成proxy的定义， 生产环境下需要配置ngxix, 比如 location 'api-cghtz 等多个入口 其实vite。config.js是开发环境下的配置走向，nginx是生产环境下， 一切都是根据axios中的base_url决定走哪个入口，走哪个后端的ip和端口， 所以需要根据每个入口的index决定控制axios中的baseurl的走向即可 ，所以给每个index配置了window?.__APP_CONFIG__?.backend 这其实相当于放弃了env的配置

---
+++ ft-checnote/backends.mjs
@@ -0,0 +1,53 @@
+export const backends = {
+    index: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, dq: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, yb: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, sb: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, ai_entry: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, safe_product: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, wz: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, watchdog: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, sbrhjy: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, cggy: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, cghtz: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, carop: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, filetest: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, fileview: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, filechunk: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    }, downloads: {
+        api: "/api",
+        url: 'http://10.8.13.66:7090'
+    },
+}
+// 未登记入口的兜底后端（绝大多数入口连它）
+export const defaultBackend = backends.index
